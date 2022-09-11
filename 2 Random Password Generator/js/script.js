33b4b1d94c9170667ee50d370ac7b8e018704ee3
Alex-Grh <alex_grh@mail.ru>
2022-09-11 15:58:53 +0400

    1

--- 2 Random Password Generator/js/script.js
+++ 2 Random Password Generator/js/script.js
@@ -1,4 +1,26 @@
-// ABCDEFGHIJKLMNOPQRSTUVWXWZ
-// abcdefghijklmnopqrstuvwxwz
-// \"$%&/()=?@~'\\.\';:+=^*_-!"
-// 0123456789
+let randomPass = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\"$%&/()=?@~`\\.\';:+=^*_-';
+let out1 = document.getElementById("out-1");
+
+function passValue(value) {
+   return Math.floor(Math.random() * value);
+}
+
+function pasGenerator() {
+   let lengthPass = document.getElementById('length-pass').value;
+   document.getElementById("length-val-pass").textContent = lengthPass;
+   let elem = '';
+
+   for (let i = 0; i < lengthPass; i++) {
+      let randomEl = passValue(randomPass.length);
+      elem += randomPass.charAt(randomEl);
+   }
+   out1.value = elem;
+
+}
+
+function copyPassword() {
+   out1.select();
+   document.execCommand('copy');
+   alert("Random password copied!");
+}
+pasGenerator();
