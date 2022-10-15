23e039072be7afe1f5d51265ee08af4d5100fafa
Alex-Grh <alex_grh@mail.ru>
2022-10-15 11:35:24 +0400

    1

--- 3 Virtual Keyboard/js/script.js
+++ 3 Virtual Keyboard/js/script.js
@@ -1,19 +1,20 @@
 let keyAll = document.querySelectorAll('.key-all');
+const textarea = document.querySelector('textarea');
 
-document.querySelector('.i-11').onkeydown = function () {
+textarea.onkeydown = function () {
    activeKeyFunk();
    capsLockFunk();
 };
 
 //-----------------------------------------------------------------------------
-document.querySelector('.i-11').onclick = function () {
+textarea.onclick = function () {
    activeKeyFunk();
    capsLockFunk();
 };
-document.querySelector('.i-11').mouseup = activeKeyFunk;
+textarea.mouseup = activeKeyFunk;
 //-----------------------------------------------------------------------------
 
-document.querySelector('.i-11').onkeyup = activeKeyFunk;
+textarea.onkeyup = activeKeyFunk;
 
 
 function activeKeyFunk() {
@@ -64,7 +65,7 @@ function capsLockFunk() {
 
 //!!!-----------------------------------------------------------------------------
 
-
+/*
 let keys = document.querySelectorAll('.keyboard_wrapper .key .row .key-all'),
    keyPad = document.querySelector('.keyboard_wrapper .key'),
    display = document.querySelector('.keyboard_wrapper .display');
@@ -95,3 +96,39 @@ if (keys && keyPad && display) {
       });
    });
 }
+
+*/
+//!!!-----------------------------------------------------------------------------
+
+
+// const buttons = document.querySelectorAll('.btn')
+// const textarea = document.querySelector('textarea');
+
+const delete_btn = document.querySelector('.delete');
+const shift_btn = document.querySelector('.shift');
+const space_btn = document.querySelector('.space');
+
+let chars = []
+
+keyAll.forEach(btn => {
+    btn.addEventListener('click', () => {
+        textarea.value += btn.innerText
+        chars = textarea.value.split('')
+    })
+})
+
+delete_btn.addEventListener('click', () => {
+    chars.pop()
+    textarea.value = chars.join('')
+})
+
+space_btn.addEventListener('click', () => {
+    chars.push(' ')
+    textarea.value = chars.join('')
+})
+
+shift_btn.addEventListener('click', () => {
+   keyAll.forEach(btn => {
+        btn.classList.toggle('upper')
+    })
+})
